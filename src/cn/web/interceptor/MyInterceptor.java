3345e6a055b3675b0a448039aa7f83d58539c745
joe.phoenix <joe.phoenix@gmail.com>
2014-05-09 16:49:00 +0800

    格式优化，以及删除需要的配置信息

--- src/cn/web/interceptor/MyInterceptor.java
+++ src/cn/web/interceptor/MyInterceptor.java
@@ -7,26 +7,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-
 public class MyInterceptor extends HandlerInterceptorAdapter {
-private int openingTime=22;
-private int closingTime=22;
-public void setOpeningTime(int openingTime) {
-this.openingTime = openingTime;
-}
-public void setClosingTime(int closingTime) {
-this.closingTime = closingTime;
-}
+	private int openingTime = 22;
+	private int closingTime = 22;
+
+	public void setOpeningTime(int openingTime) {
+		this.openingTime = openingTime;
+	}
+
+	public void setClosingTime(int closingTime) {
+		this.closingTime = closingTime;
+	}
 
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
-		System.out.println(request.getPathInfo()+" "+handler);
+		System.out.println(request.getPathInfo() + " " + handler);
 		Calendar cal = Calendar.getInstance();
 		int hour = cal.get(Calendar.HOUR_OF_DAY);
 		if (openingTime <= hour && hour < closingTime) {
 			return true;
 		} else {
-		//	response.sendRedirect("http://sina.com.cn");
+			// response.sendRedirect("http://sina.com.cn");
 			return true;
 		}
 	}
